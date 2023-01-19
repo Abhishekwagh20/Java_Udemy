a749e4477ac4eabf09da212952daab437ff4c48c
Abhishek Wagh <abhimwagh20@gmail.com>
2023-01-19 13:17:17 -0600

    Exercise 25

---
+++ Exercise_25_LargestPrime/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LargestPrime.getLargestPrime(7));
+        System.out.println(LargestPrime.getLargestPrime(21));
+        System.out.println(LargestPrime.getLargestPrime(10));
+    }
+}
